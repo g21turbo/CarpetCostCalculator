ec11775565c0a84b87dee7fd5db551ddc17f728b
g21turbo <dg21stew@gmail.com>
2023-12-06 21:06:17 -0800

    Initial commit

---
+++ src/Floor.java
@@ -0,0 +1,23 @@
+public class Floor {
+
+    private double width;
+    private double length;
+
+
+    public Floor(double width, double length) {
+        if (width < 0){
+            width = 0;
+        }
+        if (length < 0){
+            length = 0;
+        } else {
+            this.width = width;
+            this.length = length;
+        }
+    }
+
+    public double getArea() {
+        return width * length;
+    }
+
+}
